47d018fd362dcd428721525601274dd81d19915d
Matthew Farejowicz <mfarejow@mit.edu>
2020-01-07 19:23:58 -0500

    set up start

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Make me into a todo list!</div>;
+  }
+}
+
+export default App;
